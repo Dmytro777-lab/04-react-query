d0cc0029830e31735f506ea9722974c0abcb4427
Dorofeyev Dmytro <dorofeyev74@gmail.com>
2026-08-16 06:22:28 +0200

    Add React Query and pagination setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
 import App from './components/App/App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster />
+    </QueryClientProvider>
   </StrictMode>,
 );
